546a269e43bd33a861af6227ebec0339611b923d
NathDore <nathprogtravail@gmail.com>
2026-05-23 20:08:05 -0400

    style: update course, calendar and navbar colors

--- src/pages/Calendar/data/Task_data.ts
+++ src/pages/Calendar/data/Task_data.ts
@@ -2,10 +2,10 @@ import type { Task } from "../../../types/Task";
 import type { Course } from "../../../types/Course";
 import { useTaskStore } from "../../../store/taskStore";
 
-const MAT130: Course = { id: "MAT-130", name: "MAT-130", color: "#4A90D9" };
-const MAT160: Course = { id: "MAT-160", name: "MAT-160", color: "#E67E22" };
-const IFT2007: Course = { id: "IFT-2007", name: "IFT-2007", color: "#9B59B6" };
-const IFT1003: Course = { id: "IFT-1003", name: "IFT-1003", color: "#2ECC71" };
+const MAT130: Course = { id: "MAT-130", name: "MAT-130", color: "#5B8DB8" };
+const MAT160: Course = { id: "MAT-160", name: "MAT-160", color: "#C97A3E" };
+const IFT2007: Course = { id: "IFT-2007", name: "IFT-2007", color: "#8B6AAF" };
+const IFT1003: Course = { id: "IFT-1003", name: "IFT-1003", color: "#4DA57A" };
 
 export const add_fake_data = () => {
     const { addTask } = useTaskStore.getState();
