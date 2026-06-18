0be4402c5fd617c1b8c402672a9cc0d2e07a7fc4
NathDore <nathprogtravail@gmail.com>
2026-06-18 10:38:51 -0400

    fix(seedCourses): enable seedCourses before adding tasks with seedtasks

--- app/web/src/utils/seeds/seedCourses.ts
+++ app/web/src/utils/seeds/seedCourses.ts
@@ -6,7 +6,7 @@ const MAT160: Course = { id: "MAT-160", name: "MAT-160", color: "#C97A3E" };
 const IFT2007: Course = { id: "IFT-2007", name: "IFT-2007", color: "#8B6AAF" };
 const IFT1003: Course = { id: "IFT-1003", name: "IFT-1003", color: "#4DA57A" };
 
-const ENABLE_SEED: boolean = false;
+const ENABLE_SEED: boolean = true;
 
 export const COURSE_DATA: Course[] = [
     MAT130, MAT160, IFT2007, IFT1003
